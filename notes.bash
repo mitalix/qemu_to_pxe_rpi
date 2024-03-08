d2fe2122d2a3965539a8c85029bb44be35ca9b75
mitalix <v_italix@hotmail.com>
2024-03-08 15:52:59 +0100

    modified:   notes.bash

--- notes.bash
+++ notes.bash
@@ -13,10 +13,27 @@ sudo ip link del name br0 type bridge
 qemu-system-aarch64 -M raspi3b -device usb-net,netdev=net0 -netdev user,id=net0,hostfwd=tcp::2022-:22
 
 
- # ip link add br0 type bridge
- # ip tuntap add dev tap0 mode tap
- # ip link set dev tap0 master br0   # set br0 as the target bridge for tap0
- # ip link set dev eth0 master br0   # set br0 as the target bridge for eth0
- # ip link set dev br0 up
+sudo ip link add br0 type bridge
+sudo ip tuntap add dev tap0 mode tap
+sudo ip link set dev tap0 master br0   # set br0 as the target bridge for tap0
+sudo ip link set dev enp2s0 master br0   # set br0 as the target bridge for eth0
+sudo ip link set dev br0 up
+-net nic -net tap,ifname=tap0
+
+sudo ip link set dev br0 down
+sudo ip link set dev tap0 down
+sudo ip link del tap0 
+sudo ip link del br0
 
 
+sudo ip link set dev eth0 master br0   # set br0 as the target bridge for eth0
+
+./aarch64-softmmu/qemu-system-aarch64 
+-machine virt 
+-cpu cortex-a57 
+-machine type=virt 
+-nographic -smp 1 -m 2048 
+-kernel aarch64-linux-3.15rc2-buildroot.img 
+--append "console=ttyAMA0" 
+-fsdev local,id=r,path=/home/alex/lsrc/qemu/rootfs/trusty-core,security_model=none 
+-device virtio-9p-device,fsdev=r,mount_tag=r
